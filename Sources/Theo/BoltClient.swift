3549f85dc00603d37735a19b2f468679fc0c889d
Niklas Saers <niklas@saers.com>
2020-07-14 23:20:10 +0200

    Let’s call it 5.2.0

--- Sources/Theo/BoltClient.swift
+++ Sources/Theo/BoltClient.swift
@@ -37,7 +37,7 @@ open class BoltClient: ClientProtocol {
         self.password = configuration.password
         self.encrypted = configuration.encrypted
 
-        let settings = ConnectionSettings(username: self.username, password: self.password, userAgent: "Theo 4.1.0")
+        let settings = ConnectionSettings(username: self.username, password: self.password, userAgent: "Theo 5.2.0")
 
         let socket = try EncryptedSocket(hostname: hostname, port: port)
         socket.certificateValidator = UnsecureCertificateValidator(hostname: self.hostname, port: UInt(self.port))
@@ -54,9 +54,10 @@ open class BoltClient: ClientProtocol {
         self.password = password
         self.encrypted = encrypted
 
-        let settings = ConnectionSettings(username: username, password: password, userAgent: "Theo 4.1.0")
+        let settings = ConnectionSettings(username: username, password: password, userAgent: "Theo 5.2.0")
 
         let socket = try EncryptedSocket(hostname: hostname, port: port)
+        socket.certificateValidator = UnsecureCertificateValidator(hostname: self.hostname, port: UInt(self.port))
         self.connection = Connection(
             socket: socket,
             settings: settings)
