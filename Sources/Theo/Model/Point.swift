06f6ff013adeaf893d1c0b91551f183a94347893
Lewis Godowski <lewisgodowski@gmail.com>
2023-08-05 12:10:15 -0700

    Add pseudo-codable support to Structure

--- Sources/Theo/Model/Point.swift
+++ Sources/Theo/Model/Point.swift
@@ -4,44 +4,44 @@ import Foundation
 import PackStream
 
 public final class Point: ResponseItem, Sendable {
-  // MARK: - Constants & Variables
+    // MARK: - Constants & Variables
 
-  private static let srid = 4326
+    private static let srid = 4326
 
-  public let latitude: CLLocationDegrees
-  public let longitude: CLLocationDegrees
+    public let latitude: CLLocationDegrees
+    public let longitude: CLLocationDegrees
 
-  var coordinate: CLLocationCoordinate2D {
-    .init(latitude: latitude, longitude: longitude)
-  }
-
-
-  // MARK: - init
-
-  init?(data: PackProtocol) {
-    if let s = data as? Structure,
-       s.items.count >= 3,
-       s.items[0] as? Int == Point.srid,
-       let latitude = s.items[2] as? CLLocationDegrees,
-       let longitude = s.items[1] as? CLLocationDegrees
-    {
-      self.latitude = latitude
-      self.longitude = longitude
-    } else {
-      return nil
+    var coordinate: CLLocationCoordinate2D {
+        .init(latitude: latitude, longitude: longitude)
     }
-  }
 
-  init?(structure: Structure) {
-    if structure.items.count >= 3,
-       structure.items[0] as? Int == Point.srid,
-       let latitude = structure.items[2] as? CLLocationDegrees,
-       let longitude = structure.items[1] as? CLLocationDegrees
-    {
-      self.latitude = latitude
-      self.longitude = longitude
-    } else {
-      return nil
+
+    // MARK: - init
+
+    init?(data: PackProtocol) {
+        if let s = data as? Structure,
+           s.items.count >= 3,
+           s.items[0] as? Int == Point.srid,
+           let latitude = s.items[2] as? CLLocationDegrees,
+           let longitude = s.items[1] as? CLLocationDegrees
+        {
+            self.latitude = latitude
+            self.longitude = longitude
+        } else {
+            return nil
+        }
     }
-  }
+
+    init?(structure: Structure) {
+        if structure.items.count >= 3,
+           structure.items[0] as? Int == Point.srid,
+           let latitude = structure.items[2] as? CLLocationDegrees,
+           let longitude = structure.items[1] as? CLLocationDegrees
+        {
+            self.latitude = latitude
+            self.longitude = longitude
+        } else {
+            return nil
+        }
+    }
 }
--- Sources/Theo/Model/Structure+.swift
+++ Sources/Theo/Model/Structure+.swift
@@ -1,12 +1,35 @@
 import PackStream
 
 extension Structure {
-  var subType: ResponseItem.Type? {
-    if signature == 78, items.count >= 3 { return Node.self }
-    if signature == 80, items.count >= 3 { return Path.self }
-    if signature == 88, items.count >= 3 { return Point.self }
-    if signature == 82, items.count >= 5 { return Relationship.self }
-    if signature == 114, items.count >= 3 { return UnboundRelationship.self }
-    return nil
-  }
+    var subType: ResponseItem.Type? {
+        if signature == 78, items.count >= 3 { return Node.self }
+        if signature == 80, items.count >= 3 { return Path.self }
+        if signature == 88, items.count >= 3 { return Point.self }
+        if signature == 82, items.count >= 5 { return Relationship.self }
+        if signature == 114, items.count >= 3 { return UnboundRelationship.self }
+        return nil
+    }
+}
+
+extension Structure: Codable {
+    enum CodingKeys: String, CodingKey {
+        case items
+        case signature
+    }
+
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+
+        // let items = try values.decode([PackProtocol].self, forKey: .signature)
+        let signature = try values.decode(UInt8.self, forKey: .signature)
+
+        self.init(signature: signature, items: [])
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        // try container.encode(items, forKey: .items)
+        try container.encode(signature, forKey: .signature)
+    }
 }
