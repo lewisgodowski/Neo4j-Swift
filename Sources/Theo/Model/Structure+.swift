301b40762b6a3c7ea6d69b28865ef47ed18adf83
Lewis Godowski <lewisgodowski@gmail.com>
2023-08-05 15:04:35 -0700

    Move Point

--- Sources/Theo/Model/Structure+.swift
+++ Sources/Theo/Model/Structure+.swift
@@ -1,16 +1,5 @@
 import PackStream
 
-public extension Structure {
-    var subType: ResponseItem.Type? {
-        if signature == 78, items.count >= 3 { return Node.self }
-        if signature == 80, items.count >= 3 { return Path.self }
-        if signature == 88, items.count >= 3 { return Point.self }
-        if signature == 82, items.count >= 5 { return Relationship.self }
-        if signature == 114, items.count >= 3 { return UnboundRelationship.self }
-        return nil
-    }
-}
-
 extension Structure: Codable {
     enum CodingKeys: String, CodingKey {
         case items
